b3dd7386bd09fbce5b5f01c70c12846a55f746f7
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-18 12:26:26 -0400

    firstcommit

---
+++ src/CreateArrays.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class CreateArrays {
+    public static void main(String[] args){
+        // 5 Numbers
+        int[] numbers = {1, 21, 63, 4, 51};
+        System.out.println("Numbers:" + Arrays.toString(numbers));
+
+        // 7 Strings
+        String[] sevenStrings = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        System.out.println("Strings: " +Arrays.toString(sevenStrings));
+
+        //5 Characters
+        char[] userChar= {'a', 'e', 'i', 'o', 'u'};
+        System.out.println("Characters: " + Arrays.toString(userChar));
+
+        //2 Booleans
+        boolean[] bool={true, false};
+        System.out.println("Booleans: " +Arrays.toString(bool));
+
+
+
+
+
+
+
+    }
+}
